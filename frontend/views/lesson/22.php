9b54ba0dcfaf66974127780c91587508dde2d855
victorp <victor.p@zina.com.ua>
2020-12-17 17:32:14 +0200

    l22 1

--- frontend/views/lesson/22.php
+++ frontend/views/lesson/22.php
@@ -1,4 +1,109 @@
 <?php
+echo '<h2>PHP,SQL</h2><div class="d-flex-row">';
 
-echo '<h2>PHP,SQL</h2>';
 
+
+// print task number
+// print values
+function f($task, $values)
+{
+    if (empty($task)) {
+        throw new Exception('Task is tmpty', 1);
+    }
+    echo'<div class="flex-item">';
+    echo"<h3 class=\"flex-item__h3\">Task $task</h3>";
+    foreach ($values as $key => $value) {
+        echo "<p>$key</p>";
+        echo '<pre>';
+        print_r($value);
+        echo '<br>';
+        var_dump($value);
+        echo '</pre>';
+    }
+    echo'</div>';
+};
+
+date_default_timezone_set('Europe/Kiev');
+$year = date('Y') + 1;
+$result = strtotime("$year-01-01") - time();
+$result = round($result / 86400);
+f(1, ['Days before the new year' => $result]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo"</div>";
