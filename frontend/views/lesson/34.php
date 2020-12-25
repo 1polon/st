b767782cf89f0cc80d98c8b616b5e57445bb2a57
victorp <victor.p@zina.com.ua>
2020-12-25 14:00:32 +0200

    34 start

--- frontend/views/lesson/34.php
+++ frontend/views/lesson/34.php
@@ -6,6 +6,3 @@ $host = 'localhost:8080';
 $user = 'st_user';
 $password = 'st_user_password';
 $db_name = 'st';
-
-mysql_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
-phpinfo();
